16f2c3f35963326e3d734c6cf8e93a80cb10cd33
Libamlak Birhanu <libamlakbirhanu904@gmail.com>
2022-05-23 16:17:24 +0300

    modified the floating action button

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,35 @@
 <template>
   <v-app>
     <navigation :color="color" :flat="flat" />
-    <v-main class="pt-0 ">
-       <router-view></router-view>  
+    <v-main class="pt-0">
+      <router-view></router-view>
     </v-main>
-    <v-scale-transition>
-      <a style="text-decoration:none" href="https://wa.me/+251986551414" target="_blank">
-      <v-btn
-        fab
-        v-show="fab"
-        v-scroll="onScroll"
-        dark
-        fixed
-        bottom
-        right
-        color="green"
-      >
-        <v-icon large>mdi-whatsapp </v-icon>
-      </v-btn>
-      </a>
-    </v-scale-transition>
+    <v-menu transition="fab-transition">
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          fab
+          dark
+          fixed
+          bottom
+          right
+          color="green"
+          v-bind="attrs"
+          v-on="on"
+          id="fab"
+          @click="console.log('clicked')"
+        >
+          <v-icon large>mdi-whatsapp </v-icon>
+        </v-btn>
+      </template>
+      <v-card elevation="2">
+        <v-card-subtitle><v-icon>mdi-whatsapp</v-icon> check out our what's app community</v-card-subtitle>
+        <v-card-subtitle><a style="text-decoration:none" href="https://wa.me/+251986551414" target="_blank">continue to whatsapp</a></v-card-subtitle>
+      </v-card>
+    </v-menu>
     <foote />
   </v-app>
 </template>
+
 <script>
 import navigation from "./components/Navigation";
 import foote from "./components/Footer";
@@ -47,6 +54,9 @@ export default {
       this.color = "white";
       this.flat = true;
     }
+  },
+  mounted() {
+    document.getElementById('fab').click();
   },
   watch: {
     fab(value) {
